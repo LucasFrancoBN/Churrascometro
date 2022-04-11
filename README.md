7cc1ac090fa5d248cfb0a682134debec2403ce4f
Lucas <lucasfbnavarro@gmail.com>
2022-04-11 14:24:22 -0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# Churrascometro
+<h1>Verificador de Idade</h1>
+
+<!-- LINKS -->
+<div align="center">
  
-<h3 align="center"> 🚧 Projeto Churrascometro 🚀 em construção 🚧 </h3>
+ <h2> Sumário</h2>
+ 
+ <a href="#fotos">Fotos</a> - 
+  <a href="#GIFs">GIFs</a> - 
+  <a href="#sobre">Sobre o Projeto</a> - 
+  <a href="#tec">Tecnologias</a>
+</div>
+<br>
+
+<!-- FOTOS -->
+<div id="fotos">
+    <h2> 📸 Fotos </h2>
+        <img src="./assets/churrascometro.png" alt="" style="width:750px">
+        <br><br>
+       
+ <h2> 📱 Responsivo </h2>
+    <img src="./assets/responsivo.png" alt="" style="height:500px">
+        <br><br>
+        
+   <h2 id="GIFs"> 🎥 GIFs </h2>
+        <img src="./assets/projetoGif.gif" alt="" style="width:750px">
+        <br><br>
+<br><br>
+
+</div>
+
+<!-- SOBRE -->
+<div id="sobre">
+    <h2> 📝 Sobre o Projeto </h2> 
+    <p> Esse projeto fará com que você e sua família tenham um churrasco com a quandtidade certa de carne e bebida. Ele leva em conta que um adulto consuma em média 400g de carne e beba 1,3 Litros de cerveja; leva em conta que uma criança consuma cerca de 200g de carne e 600ml de refrigerante; além disso, caso a duração do churrasco seja maior do que 5 horas, os valores irão aumentar automaticamente</p>
+ 
+ <h3 align="center">Link do Projeto <a href="https://lucasfrancobn.github.io/Verificador-de-Idade/">CLIQUE AQUI</a></h3>
+
+</div>
+<br>
+
+<!-- TECNOLOGIAS -->
+<div id="tec">
+
+<h2> 🖥️ Tecnologias</h2>
+    <p> ✔️ HTML 5 </p>
+    <p> ✔️ CSS 3 </p>
+    <p> ✔️ JavaScript </p>
+
+</div>
--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,6 @@
 // carne/adulto 400g | + 5 h 600g
 // cerveja 1300 ml | + 5 h 2250 ml
-// refrigerante 100ml | + 5 h 1500 ml
+// refrigerante 750ml | + 5 h 1125 ml
 // crainca =  0.5
 
 let inputAdultos = document.getElementById("adultos");
@@ -21,9 +21,9 @@ function calcular() {
     let cervejaTotal = adultos*cervejaPP(duracao);
     let reTotal = adultos*rePP(duracao) + (criancas*rePP(duracao))/2
 
-    resultado.innerHTML = `<div style="margin-bottom: 25px"><img src="./assets/bife.png" style="float: left"><p style="margin-left:35px">${Math.ceil(carneTotal/1000)}Kg de carne</p></div>`;
-    resultado.innerHTML += `<div style="margin-bottom: 25px"><img src="./assets/beer.png" style="float: left"><p style="margin-left:35px">${Math.ceil(cervejaTotal/350)} Latinhas de cerveja</p></div>`;
-    resultado.innerHTML += `<div style="margin-bottom: 25px"><img src="./assets/soda.png" style="float: left"><p style="margin-left:35px">${Math.ceil(reTotal/1000)}L de refrigerante</p></div>`;
+    resultado.innerHTML = `<div style="display: flex; justify-content: flex-start; align-items: center; margin-bottom: 15px;"><img src="./assets/bife.png" style="float: left"><p style="margin-left:35px">${Math.ceil(carneTotal/1000)}Kg de carne</p></div>`;
+    resultado.innerHTML += `<div style="display: flex; justify-content: flex-start; align-items: center; margin-bottom: 15px;"><img src="./assets/beer.png" style="float: left"><p style="margin-left:35px">${Math.ceil(cervejaTotal/350)} Latinhas de cerveja</p></div>`;
+    resultado.innerHTML += `<div style="display: flex; justify-content: flex-start; align-items: center; margin-bottom: 15px;"><img src="./assets/soda.png" style="float: left"><p style="margin-left:35px">${Math.ceil(reTotal/600)}L de refrigerante</p></div>`;
 }
 
 function carnePP(duracao) {
@@ -44,8 +44,8 @@ function cervejaPP(duracao) {
 
 function rePP(duracao) {
     if (duracao >= 5) {
-        return 1500
+        return 1125
     } else {
-        return 1000
+        return 600
     }
 }
